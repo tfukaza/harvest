b8c3a3de4fe9056d3ca45d720388bf6b8fd3e29e
tfukaza <tfukaza@users.noreply.github.com>
2021-06-29 22:06:55 +0000

    deploy: 3a4dad79877e58110add2802415273b41764fd6b

--- harvest/plugin/__init__.py
+++
@@ -1,1 +0,0 @@
-from harvest.plugin.yahoo import *
--- harvest/plugin/_base.py
+++
@@ -1,9 +0,0 @@
-class Plugin:
-    """
-    As the name implies, it implements funtionalities that the Trader class
-    alone cannot support, like retrieving earning report dates.  
-
-    The name of the plugin will follow thge format:
-        {Name of source}{Functionality added}Plugin
-    """
-    pass 
