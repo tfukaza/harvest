e0b996fe1158c22891db3b1af308242db887f993
tfukaza <tfukaza@users.noreply.github.com>
2021-06-30 05:21:08 +0000

    deploy: 7dd82482b3a2c0be20a610203dfde5a0b870f9ea

---
+++ _next/static/Ebr7QFJUsTtyp00G122aq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e7f77ac02a0054325bba.css","static/chunks/pages/index-6300f4d190608eaba5dc.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
