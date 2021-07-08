05bba63f5660e372315d88ba828e178958b204ed
Tomoki Fukazawa <tomokifukazawa123@gmail.com>
2021-07-07 17:24:46 -0700

    Bug fixes

--- website/pages/components/footer.js
+++ website/pages/components/footer.js
@@ -1,12 +1,13 @@
 import styles from '../../styles/Home.module.scss'
 import Link from 'next/link'
+import StaticImage from './static_image.js'
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <section className={styles.section}>
             <div>
-                <img src="/wordmark.svg"></img>
+                <StaticImage src="/wordmark.svg" />
                 <Link href="https://github.com/tfukaza/harvest/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5B%F0%9F%AA%B0BUG%5D">Report a Bug</Link>
                 <Link href="https://github.com/tfukaza/harvest/issues/new?assignees=&labels=enhancement%2C+question&template=feature-request.md&title=%5B%F0%9F%92%A1Feature+Request%5D">Suggest a Feature</Link>
             </div>
--- website/pages/components/static_image.js
+++ website/pages/components/static_image.js
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-
 const my_loader = ({ src, }) => {
   return `{${process.env.BASE_PATH}/${src}}`
 }
 
 export default function StaticImage({props, src}) {
-  return (
-    <img src={my_loader(src)} alt="img" />
-  )
-}
+    if (src === undefined) return (<img />)
+    return (
+        <img src={my_loader(src)} alt="img" />
+    )
+}   
