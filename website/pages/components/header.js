e4ae60a3860339f6c4809bfddfab6cf6b1063eae
Tomoki Fukazawa <tomokifukazawa123@gmail.com>
2021-07-07 17:43:46 -0700

    Bug fixes

--- website/pages/components/header.js
+++ website/pages/components/header.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
+import StaticImage from './static_image'
 
 export default function Header({c, title}) {
+    let base = process.env.BASE_PATH;
+    if (base === undefined) base = '';
     return (
         <Head>
             <title>{title}</title>
@@ -8,7 +11,7 @@ export default function Header({c, title}) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta property="og:title" content={title}/>
             <meta property="og:type" content="website"/>
-            <meta property="og:image" content="/img/wordmark.svg"/>
+            <meta property="og:image" content={base + '/wordmark.svg'}/>
             <link rel="icon" href="/favicon.ico" />
         </Head>
     )
