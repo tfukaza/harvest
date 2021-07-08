a6c6a88f014b0d9cb2e35476bec499ef996d1825
Tomoki Fukazawa <tomokifukazawa123@gmail.com>
2021-07-07 17:39:20 -0700

    Bug fixes

--- website/pages/components/static_image.js
+++ website/pages/components/static_image.js
@@ -1,7 +1,7 @@
 const my_loader = (src) => {
     let base = process.env.BASE_PATH;
     if (base === undefined) base = '';
-    return `{${base}/${src}}`
+    return `${base}/${src}`
 }
 
 export default function StaticImage({props, src}) {
