dd15a321f43413262165c0a30eb3dccd67124a3c
Tomoki Fukazawa <tomokifukazawa123@gmail.com>
2021-07-16 19:16:03 -0700

    Improved tests and fixed bug in timeout system

--- test/test_trader.py
+++ test/test_trader.py
@@ -8,6 +8,10 @@ from harvest.api.dummy import DummyStreamer
 from harvest.api.paper import PaperBroker
 #from harvest.api.robinhood import Robinhood
 
+import datetime as dt
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 from harvest.utils import gen_data
 
@@ -60,34 +64,49 @@ class TestTrader(unittest.TestCase):
         t.start('1MIN', kill_switch=True)
 
         # Save the last datapoint of B
-        b_data = t.storage.load('B', '1MIN')['B']['close'][-1]
+        a_cur = t.storage.load('A', '1MIN')
+        b_cur = t.storage.load('B', '1MIN')
         # Only send data for A
-        data = {
-            'A': gen_data('A', 1)
-        }
+        data = gen_data('A', 1)
+        data.index=[a_cur.index[-1]+dt.timedelta(minutes=1)]
+        data = {'A': data}
+       
         t.main(data)
         # Wait for the timeout
         time.sleep(2)
+
+        # Check if A has been added to storage
+        self.assertEqual(a_cur['A']['close'][-1],       t.storage.load('A', '1MIN')['A']['close'][-2])
+        self.assertEqual(data['A']['A']['close'][-1],   t.storage.load('A', '1MIN')['A']['close'][-1])
         # Check if B has been duplicated
-        self.assertEqual(b_data, t.storage.load('B', '1MIN')['B']['close'][-2])
-        self.assertEqual(b_data, t.storage.load('B', '1MIN')['B']['close'][-1])
+        self.assertEqual(b_cur['B']['close'][-1],       t.storage.load('B', '1MIN')['B']['close'][-2])
+        self.assertEqual(b_cur['B']['close'][-1],       t.storage.load('B', '1MIN')['B']['close'][-1])
     
     def test_timeout_cancel(self):
         t = trader.Trader()
         t.set_symbol(['A', 'B'])
         t.start('1MIN', kill_switch=True)
 
-        a_new = gen_data('A', 1)
-        b_new = gen_data('B', 1)
+        # Save the last datapoint of B
+        a_cur = t.storage.load('A', '1MIN')
+        b_cur = t.storage.load('B', '1MIN')
         # Only send data for A
-        data = {'A': a_new}
-        t.main(data)
-        time.sleep(0.3)
-        data = {'B': b_new}
-        t.main(data)
-        # Check that the new data has been stored
-        self.assertEqual(a_new['A']['close'][-1], t.storage.load('A', '1MIN')['A']['close'][-1])
-        self.assertEqual(b_new['B']['close'][-1], t.storage.load('B', '1MIN')['B']['close'][-1])
+        data_a = gen_data('A', 1)
+        data_a.index=[a_cur.index[-1]+dt.timedelta(minutes=1)]
+        data_a = {'A': data_a}
+        data_b = gen_data('B', 1)
+        data_b.index=[b_cur.index[-1]+dt.timedelta(minutes=1)]
+        data_b = {'B': data_b}
+        t.main(data_a)
+        # Wait 
+        time.sleep(0.1)
+        t.main(data_b)
 
+        # Check if A has been added to storage
+        self.assertEqual(a_cur['A']['close'][-1],       t.storage.load('A', '1MIN')['A']['close'][-2])
+        self.assertEqual(data_a['A']['A']['close'][-1],   t.storage.load('A', '1MIN')['A']['close'][-1])
+        # Check if B has been added to storage
+        self.assertEqual(b_cur['B']['close'][-1],       t.storage.load('B', '1MIN')['B']['close'][-2])
+        self.assertEqual(data_b['B']['B']['close'][-1],   t.storage.load('B', '1MIN')['B']['close'][-1])
 if __name__ == '__main__':
     unittest.main()
