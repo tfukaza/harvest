246bc8fb4d4096b90821fb1a19b957ad0ec13cb1
Sher Shah <sher.shah010@berkeley.edu>
2021-07-25 19:32:22 -0700

    fixed bugs

--- harvest/api/dummy.py
+++ harvest/api/dummy.py
@@ -24,8 +24,11 @@ class DummyStreamer(API):
     def __init__(self, path: str=None, now: dt.datetime=default_now):
         self.trader = None
 
+        # Set the current time
         self._set_now(now)
+        # Used so `fetch_price_history` can work without running `setup`
         self.interval = self.interval_list[0]
+        # Store random values and generates for each asset tot make `fetch_price_history` fixed
         self.randomness = {}
 
     def setup(self, watch: List[str], interval, trader=None, trader_main=None):
@@ -69,27 +72,11 @@ class DummyStreamer(API):
                 results[symbol] = self.fetch_price_history(symbol, self.interval, last, today).iloc[[-1]]
         return results
 
-
-    # def _generate_fake_stock_data(self):
-    #     """
-    #     Generates fake open, close, low, high, and volume data points for stock and crypto data.
-    #     Each data point is dependent on the previous one and there is no bias to whether the
-    #     stock will go up or down over time. Uses a seed and timestamp to make the PRNG predictable.
-    #     """
-
-    #     open_s = random.uniform(2, 1000)
-    #     volume = random.randint(1, 1e7)
-
-    #     while True: 
-    #         open_s = max(open_s + random.uniform(-1, 1), 0.001)
-    #         close = open_s + random.uniform(-1, 1)
-    #         low = max(min(open_s, close) - random.uniform(0.001, 1), 0)
-    #         high = max(open_s, close) + random.uniform(0.001, 1)
-    #         volume = max(volume + random.randint(-5, 5), 1)  
-    #         yield open_s, high, low, close, volume
-
     def _set_now(self, current_datetime: dt.datetime) -> None:
-        self.now = pytz.utc.localize(current_datetime)
+        if current_datetime.tzinfo is None or current_datetime.tzinfo.utcoffset(current_datetime) is None:
+            self.now = pytz.utc.localize(current_datetime)
+        else: 
+            self.now = current_datetime
 
     def _tick(self) -> None:
         self.now += interval_to_timedelta(self.interval)
@@ -110,37 +97,67 @@ class DummyStreamer(API):
                 start = self.now - dt.timedelta(days=31)
             else:
                 start = self.now - dt.timedelta(days=365)
+
         if end is None:
             end = self.now
 
+        if start.tzinfo is None or start.tzinfo.utcoffset(start) is None:
+            start = pytz.utc.localize(start)
+
+        if end.tzinfo is None or end.tzinfo.utcoffset(end) is None:
+            end = pytz.utc.localize(end)
+
+        # Convert datetime to indices
         start_index = int((start - epoch_zero()).total_seconds() // 60)
         end_index = 1 + int((end - epoch_zero()).total_seconds() // 60)
 
         if symbol in self.randomness:
+            # If we already generated data from this asset
+
             num_of_random = 1 + end_index
 
             if len(self.randomness[symbol]) < num_of_random:
-                rng = self.randomness[symbol + '_rng']
-                increments = rng(num_of_random - len(self.randomness[symbol]))
+                # If the new end index is greater than the data we have
+
+                # Get the rng for this symbol
+                rng = self.randomness[symbol + '_rng'] 
+
+                # Generate a bunch of random numbers from -0.5 to 0.5
+                increments = rng.random(num_of_random - len(self.randomness[symbol])) - 0.499
+
+                # Get sum of the previous price changes
                 latest_price_change = self.randomness[symbol][-1]
-                new_price_changes = latest_price_change + np.cumsum(increments)
+
+                # Calculate the change in price since the first price
+                new_price_changes = latest_price_change + np.cumsum(increments) 
+
+                # Store the new prices
                 self.randomness[symbol] = np.append(self.randomness[symbol], new_price_changes)
             
         else:
+            # If there is no information about the asset 
+
+            # Create an rng using the asset's symbol as a seed
             rng = np.random.default_rng(int.from_bytes(symbol.encode('ascii'), 'big'))
             num_of_random = 1 + end_index
-            increments = rng.random(num_of_random) - 0.5
+            increments = rng.random(num_of_random) - 0.499
+
+            # Store the price change since the first price
             self.randomness[symbol] = np.cumsum(increments)
             self.randomness[symbol + '_rng'] = rng
 
+        # The inital price is arbitarly calculated from the first change in price
         start_price = 100 * (self.randomness[symbol][0] + 0.51)     
 
         times = []
         current_time = start
 
+        # Get the prices for the current interval
         prices = start_price + self.randomness[symbol][start_index:end_index]
+        # Prevent prices from going negative
         prices[prices < 0] = 0.01
 
+        # Calculate olhcv from the prices
         open_s = 0.95 * prices
         low = 0.8 * prices 
         high = 1.2 * prices 
@@ -148,7 +165,7 @@ class DummyStreamer(API):
         volume = (1000 * (prices + 20)).astype(int)
 
 
-        # Fake the data 
+        # Fake the timestamps 
         while current_time <= end:
             times.append(current_time)
             current_time += dt.timedelta(minutes=1)
