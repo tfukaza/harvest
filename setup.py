97b09934763cc1a8972facb28a4a255613f0edbd
Sher Shah <sher.shah010@berkeley.edu>
2021-08-12 23:16:01 -0700

    minor bug fixes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class CoverageTestCMD(Command):
         pass
 
     def run(self):
-        subprocess.run(['coverage', 'run', '--source', 'harvest', '-m', 'unittest', 'discover', '-s', 'tests'])
+        subprocess.run(['coverage', 'run', '--source', 'harvest', '--omit', 'harvest/api/robinhood.py,harvest/api/alpaca.py', '-m', 'unittest', 'discover', '-s', 'tests'])
         subprocess.run(['coverage', 'report'])
         subprocess.run(['coverage', 'html'])
         
