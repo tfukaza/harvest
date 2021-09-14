6f8d8f44375a1c0e2402805458871b7a75a51c96
Tomoki Fukazawa <tomokifukazawa123@gmail.com>
2021-09-13 23:19:44 -0700

    Clean up Tester class code

--- harvest/trader/tester.py
+++ harvest/trader/tester.py
@@ -1,6 +1,5 @@
 # Builtins
 import datetime as dt
-from logging import critical, error, info, warning, debug
 from typing import Any, Dict, List, Tuple
 import os.path
 from pathlib import Path
@@ -25,14 +24,11 @@ class BackTester(trader.Trader):
     on historical data. 
     """
 
-    def __init__(self, streamer=None, config={}):      
+    def __init__(self, streamer=None, config={}):
         """Initializes the TestTrader. 
         """
         
-        if streamer == None:
-            self.streamer = YahooStreamer()
-        else:
-            self.streamer = streamer
+        self.streamer = YahooStreamer() if streamer is None else streamer
         self.broker = PaperBroker()
 
         self.watchlist_global = []             # List of stocks to watch
@@ -48,135 +44,6 @@ class BackTester(trader.Trader):
         self.order_queue = []       # Queue of unfilled orders 
 
         self.logger = BaseLogger()
-
-
-    def read_pickle_data(self):
-        """Function to read backtesting data from a local file. 
-
-        :interval: The interval of the data
-        :path: Path to the local data file
-        :date_format: The format of the data's timestamps
-        """
-        for s in self.watchlist_global:
-            for i in [self.interval[s]["interval"]] + self.interval[s]["aggregations"]:
-                df = self.storage.open(s, i).dropna()
-                if df.empty or now() - df.index[-1] > dt.timedelta(days=1):
-                    warning(f"Running FETCH")
-                    df = self.streamer.fetch_price_history(s, i).dropna()
-                self.storage.store(s, i, df)
-
-    def read_csv_data(self, path: str, date_format: str='%Y-%m-%d %H:%M:%S'):
-        """Function to read backtesting data from a local CSV file. 
-
-        :interval: The interval of the data
-        :path: Path to the local data file
-        :date_format: The format of the data's timestamps
-        """
-        for s in self.watchlist_global:
-            for i in [self.interval[s]["interval"]] + self.interval[s]["aggregations"]:
-                df = self.read_csv(f"{path}/{s}-{interval_enum_to_string(i)}.csv").dropna()
-                if df.empty:
-                    warning(f"Running FETCH")
-                    df = self.streamer.fetch_price_history(s, i).dropna()
-                self.storage.store(s, self.interval, df)
-    
-    def read_csv(self, path:str) -> pd.DataFrame:
-        """Reads a CSV file and returns a Pandas DataFrame. 
-
-        :path: Path to the CSV file. 
-        """
-        if not os.path.isfile(path):
-            return pd.DataFrame()
-        df = pd.read_csv(path)
-        df = df.set_index(['timestamp'])
-        if isinstance(df.index[0], str):
-            df.index = pd.to_datetime(df.index)
-        else:
-            df.index = pd.to_datetime(df.index, unit='s')
-        df = df[["open", "high", "low", "close", "volume"]].astype(float)
-        df = df.sort_index()
-        return df
-            
-    def _setup(self, source, interval:str, aggregations=None, path=None):
-        
-        self._setup_params(interval, aggregations)
-        self._setup_account()
-
-        self.df = {}
-
-        self.storage.limit_size = False
-        if source == "PICKLE":
-            self.read_pickle_data()
-        elif source == "CSV":
-            self.read_csv_data(path)
-        else:
-            raise Exception(f"Invalid source {source}. Must be 'PICKLE' or 'CSV'")
-       
-        conv = {
-            Interval.MIN_1: 1,
-            Interval.MIN_5: 5,
-            Interval.MIN_15: 15,
-            Interval.MIN_30: 30,
-            Interval.HR_1: 60,
-            Interval.DAY_1: 1440
-        }
-
-        # TODO: Skip the following step if aggregated data already exists
-
-        # Generate the "simulated aggregation" data
-        for sym in self.interval:
-            interval = self.interval[sym]["interval"]
-            interval_txt = interval_enum_to_string(interval)
-            df = self.storage.load(sym, interval_txt)
-            rows = len(df.index)
-            print(f"Formatting {sym} data...")
-            for agg in self.interval[sym]["aggregations"]:
-                agg_txt = interval_enum_to_string(agg)
-                tmp_path = f"{path}/{sym}-{interval_txt}+{agg_txt}.pickle"
-                file = Path(tmp_path)
-                if file.is_file():
-                    continue
-                print(f"Formatting aggregation from {interval_txt} to {agg_txt}...")
-                points = int(conv[agg]/conv[interval])
-                for i in tqdm(range(rows)):
-                    df_tmp = df.iloc[0:i+1]                    
-                    df_tmp = df_tmp.iloc[-points:] 
-                    agg_df = aggregate_df(df_tmp, agg)
-                    self.storage.store(sym, interval_txt+'+'+agg_txt, agg_df.iloc[[-1]], remove_duplicate=False)
-        print("Formatting complete")
-
-        # # Save the current state of the queue
-        # for s in self.watch:
-        #     self.load.append_entry(s, self.interval, self.storage.load(s, self.interval))
-        #     for i in self.aggregations:
-        #         self.load.append_entry(s, '-'+i, self.storage.load(s, '-'+i), False, True)
-        #         self.load.append_entry(s, i, self.storage.load(s, i))
-
-        # Move all data to a cached dataframe
-        for i in [self.interval] + self.aggregations:
-            i = i if i == self.interval else self.interval+'+'+i
-            self.df[i] = {}
-            for s in self.watch:
-                df = self.storage.load(s, i, no_slice=True)
-                self.df[i][s] = df.copy() 
-        
-        # Trim data so start and end dates match between assets and intervals
-        # data_start = pytz.utc.localize(dt.datetime(1970, 1, 1))
-        # data_end = pytz.utc.localize(dt.datetime.utcnow().replace(microsecond=0, second=0))
-        # for i in [self.interval] + self.aggregations:
-        #     for s in self.watch:
-        #         start = self.df[i][s].index[0]
-        #         end = self.df[i][s].index[-1]
-        #         if start > data_start:
-        #             data_start = start
-        #         if end < data_end:
-        #             data_end = end
-
-        # for i in [self.interval] + self.aggregations:
-        #     for s in self.watch:
-        #         self.df[i][s] = self.df[i][s].loc[data_start:data_end]
-            
-        self.load_watch = True
 
     def start(self, interval: str='5MIN', aggregations: List[Any]=[], source: str='PICKLE', path: str="./data", kill_switch: bool=False):
         """Runs backtesting. 
@@ -204,13 +71,141 @@ class BackTester(trader.Trader):
         for a in self.algo:
             a.setup()
             a.trader = self
-            a.watch = self.watch
-            a.fetch_interval = self.fetch_interval
             
         self.main(interval)
+    
+    def _setup(self, source, interval:str, aggregations=None, path=None):
+        self._setup_params(interval, aggregations)
+        self._setup_account()
+
+        self.df = {}
+
+        self.storage.limit_size = False
+        
+        if source == "PICKLE":
+            self.read_pickle_data()
+        elif source == "CSV":
+            self.read_csv_data(path)
+        else:
+            raise Exception(f"Invalid source {source}. Must be 'PICKLE' or 'CSV'")
+       
+        conv = {
+            Interval.MIN_1: 1,
+            Interval.MIN_5: 5,
+            Interval.MIN_15: 15,
+            Interval.MIN_30: 30,
+            Interval.HR_1: 60,
+            Interval.DAY_1: 1440
+        }
+
+        # Generate the "simulated aggregation" data
+        for sym in self.interval:
+            interval = self.interval[sym]["interval"]
+            interval_txt = interval_enum_to_string(interval)
+            df = self.storage.load(sym, interval)
+            df_len = len(df.index)
+
+            print(f"Formatting {sym} data...")
+            for agg in self.interval[sym]["aggregations"]:
+                agg_txt = interval_enum_to_string(agg)
+                tmp_path = f"{path}/{sym}-{interval_txt}+{agg_txt}.pickle"
+                file = Path(tmp_path)
+                if file.is_file():
+                    continue
+                    # TODO: check if file is updated with latest data
+                print(f"Formatting aggregation from {interval_txt} to {agg_txt}...")
+                points = int(conv[agg]/conv[interval])
+                for i in range(df_len):
+                    #df_timestamp = df.index[i]
+                    df_tmp = df.iloc[0:i+1]                    
+                    df_tmp = df_tmp.iloc[-points:] # Only get recent data, since aggregating the entire df will take too long
+                    agg_df = aggregate_df(df_tmp, agg)
+                    self.storage.store(sym, agg-16, agg_df.iloc[[-1]], remove_duplicate=False)
+        print("Formatting complete")
+
+        # # Save the current state of the queue
+        # for s in self.watch:
+        #     self.load.append_entry(s, self.interval, self.storage.load(s, self.interval))
+        #     for i in self.aggregations:
+        #         self.load.append_entry(s, '-'+i, self.storage.load(s, '-'+i), False, True)
+        #         self.load.append_entry(s, i, self.storage.load(s, i))
+
+        # Move all data to a cached dataframe
+        for sym in self.interval:
+            self.df[sym] = {}
+            inter = self.interval[sym]["interval"]
+            df = self.storage.load(sym, inter, no_slice=True)
+            self.df[sym][inter] = df.copy() 
+
+            for agg in self.interval[sym]["aggregations"]:
+                agg = agg-16
+                df = self.storage.load(sym, agg, no_slice=True)
+                self.df[sym][agg] = df.copy() 
+        
+        # Trim data so start and end dates match between assets and intervals
+        # data_start = pytz.utc.localize(dt.datetime(1970, 1, 1))
+        # data_end = pytz.utc.localize(dt.datetime.utcnow().replace(microsecond=0, second=0))
+        # for i in [self.interval] + self.aggregations:
+        #     for s in self.watch:
+        #         start = self.df[i][s].index[0]
+        #         end = self.df[i][s].index[-1]
+        #         if start > data_start:
+        #             data_start = start
+        #         if end < data_end:
+        #             data_end = end
+
+        # for i in [self.interval] + self.aggregations:
+        #     for s in self.watch:
+        #         self.df[i][s] = self.df[i][s].loc[data_start:data_end]
+            
+        self.load_watch = True
+    
+    def read_pickle_data(self):
+        """Function to read backtesting data from a local file. 
+
+        :interval: The interval of the data
+        :path: Path to the local data file
+        :date_format: The format of the data's timestamps
+        """
+        for s in self.interval:
+            for i in [self.interval[s]["interval"]] + self.interval[s]["aggregations"]:
+                df = self.storage.open(s, i).dropna()
+                if df.empty or now() - df.index[-1] > dt.timedelta(days=1):
+                    df = self.streamer.fetch_price_history(s, i).dropna()
+                self.storage.store(s, i, df)
+
+    def read_csv_data(self, path: str, date_format: str='%Y-%m-%d %H:%M:%S'):
+        """Function to read backtesting data from a local CSV file. 
+
+        :interval: The interval of the data
+        :path: Path to the local data file
+        :date_format: The format of the data's timestamps
+        """
+        for s in self.interval:
+            for i in [self.interval[s]["interval"]] + self.interval[s]["aggregations"]:
+                df = self.read_csv(f"{path}/{s}-{interval_enum_to_string(i)}.csv").dropna()
+                if df.empty:
+                    df = self.streamer.fetch_price_history(s, i).dropna()
+                self.storage.store(s, self.interval, df)
+    
+    def read_csv(self, path:str) -> pd.DataFrame:
+        """Reads a CSV file and returns a Pandas DataFrame. 
+
+        :path: Path to the CSV file. 
+        """
+        if not os.path.isfile(path):
+            return pd.DataFrame()
+        df = pd.read_csv(path)
+        df = df.set_index(['timestamp'])
+        if isinstance(df.index[0], str):
+            df.index = pd.to_datetime(df.index)
+        else:
+            df.index = pd.to_datetime(df.index, unit='s')
+        df = df[["open", "high", "low", "close", "volume"]].astype(float)
+        df = df.sort_index()
+        return df
 
     def main(self):
-        info("Running test...")
 
         # import cProfile
         # pr = cProfile.Profile()
@@ -219,7 +214,7 @@ class BackTester(trader.Trader):
 
         common_start = None
         common_end = None
-        for s in self.watchlist_global:
+        for s in self.interval:
             for i in [self.interval[s]["interval"]] + self.interval[s]["aggregations"]:
                 if common_start is None or self.df[i][s].index[0] > common_start:
                     common_start = self.df[i][s].index[0]
@@ -229,16 +224,24 @@ class BackTester(trader.Trader):
 
         self.storage.limit_size = True
 
+        # TODO handle edge case where starting times are not aligned
+
+        for s in self.interval:
+            inter = self.interval[s]["interval"]
+            start_index = self.df[s][inter].index.index(common_start)
+            self.interval[s]["start"] = start_index
+
         self.timestamp = common_start
+        # TODO: assign counter to each symbol
+        counter = 0
 
         while self.timestamp < common_end:
 
             df_dict = {}
             for sym in self.interval:
                 inter = self.interval[sym]["interval"]
-                inter_txt = interval_enum_to_string(inter)
                 if is_freq(self.timestamp, inter):
-                    data = self.df[inter_txt][sym].loc[self.timestamp].iloc[-1] 
+                    data = self.df[sym][inter].loc[self.timestamp]
                     df_dict[sym] = data
 
             update = self._update_order_queue()
@@ -246,14 +249,12 @@ class BackTester(trader.Trader):
             
             for sym in self.interval:
                 inter = self.interval[sym]["interval"]
-                inter_txt = interval_enum_to_string(inter)
                 if is_freq(self.timestamp, inter):
-                    df = self.df[inter_txt][sym].loc[self.timestamp].iloc[-1] 
-                    self.storage.store(s, inter_txt, df, save_pickle=False)
+                    df = self.df[inter][sym].loc[self.timestamp]
+                    self.storage.store(s, inter, df, save_pickle=False)
                     # Add data to aggregation queue
                     for agg in self.interval[sym]["aggregations"]:
-                        agg_txt = interval_enum_to_string(agg)
-                        df = self.df[inter_txt+'+'+agg_txt][s].loc[self.timestamp]
+                        df = self.df[s][agg-16].iloc[self.interval[sym]["start"]+counter]
                         self.storage.store(s, agg, df)
 
             new_algo = []
@@ -279,7 +280,6 @@ class BackTester(trader.Trader):
 
         print(self.account)
 
- 
     def _queue_update(self, new_df: pd.DataFrame, time):
         pass
 
