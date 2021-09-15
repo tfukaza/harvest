dea44850419aa461d8ad82fd9dc7d56e34cfdf1f
Sher Shah <sher.shah010@berkeley.edu>
2021-09-14 20:12:52 -0700

    remove build dir

--- build/lib/harvest/storage/__init__.py
+++
@@ -1,5 +0,0 @@
-from harvest.storage.base_storage import BaseStorage
-from harvest.storage.csv_storage import CSVStorage
-from harvest.storage.pickle_storage import PickleStorage
-
-from harvest.storage.base_logger import BaseLogger
--- build/lib/harvest/trader/__init__.py
+++
@@ -1,2 +0,0 @@
-from harvest.trader.trader import Trader
-from harvest.trader.tester import BackTester
