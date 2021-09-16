e9714a5a89a688bfb3330978482c46cba1f0cb7f
Tomoki Fukazawa <tomokifukazawa123@gmail.com>
2021-09-15 20:51:07 -0700

    Fixed merge

--- setup.py
+++ setup.py
@@ -14,17 +14,12 @@ class LintCMD(Command):
         self.check = self.check == 'True'
 
     def run(self):
-<<<<<<< HEAD
-        subprocess.run(["black", "harvest"])
-
-=======
         command = ['black', 'harvest']
 
         if self.check:
             command.append('--check')
 
         exit(subprocess.run(command).returncode)
->>>>>>> main
 
 class CoverageTestCMD(Command):
     user_options = []
@@ -36,31 +31,10 @@ class CoverageTestCMD(Command):
         pass
 
     def run(self):
-<<<<<<< HEAD
-        subprocess.run(
-            [
-                "coverage",
-                "run",
-                "--source",
-                "harvest",
-                "--omit",
-                "harvest/api/robinhood.py,harvest/api/alpaca.py",
-                "-m",
-                "unittest",
-                "discover",
-                "-s",
-                "tests",
-            ]
-        )
-        subprocess.run(["coverage", "report"])
-        subprocess.run(["coverage", "html"])
-
-=======
         a = subprocess.run(['coverage', 'run', '--source', 'harvest', '--omit', 'harvest/api/robinhood.py,harvest/api/alpaca.py', '-m', 'unittest', 'discover', '-s', 'tests']).resturncode
         b = subprocess.run(['coverage', 'report']).returncode
         c = subprocess.run(['coverage', 'html']).returncode
         exit(a + b + c)
->>>>>>> main
 
 setup(
     cmdclass={
