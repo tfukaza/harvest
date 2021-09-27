e94408e1043ed154a4e8f7b88519b66a18d81fd4
Mike M <mikeones42@gmail.com>
2021-09-26 22:24:29 -0400

    Update test_api_webull.py

--- tests/test_api_webull.py
+++ tests/test_api_webull.py
@@ -5,7 +5,11 @@ import datetime as dt
 import os
 
 from harvest.utils import not_gh_action
+from harvest.api.paper import PaperBroker
+from harvest.api.dummy import DummyStreamer
 from harvest.api.webull import Webull
+from harvest.utils import *
+
 
 class TestWebull(unittest.TestCase):
     def not_gh_action(func):
@@ -19,7 +23,17 @@ class TestWebull(unittest.TestCase):
     @not_gh_action
     def test_fetch_prices(self):
         wb = Webull()
-        df = wb.fetch_price_history("SPY", interval="1MIN")["SPY"]
+        interval = {
+            "@BTC": {"interval": Interval.MIN_1, "aggregations": []},
+            "SPY": {"interval": Interval.MIN_1, "aggregations": []},
+        }
+        wb.setup(interval)
+        df = wb.fetch_price_history("SPY", interval=Interval.MIN_1)["SPY"]
+        self.assertEqual(
+            sorted(list(df.columns.values)),
+            sorted(["open", "high", "low", "close", "volume"]),
+        )
+        df = wb.fetch_price_history("BTC", interval=Interval.MIN_1)["BTC"]
         self.assertEqual(
             sorted(list(df.columns.values)),
             sorted(["open", "high", "low", "close", "volume"]),
@@ -28,23 +42,29 @@ class TestWebull(unittest.TestCase):
     @not_gh_action
     def test_setup(self):
         wb = Webull()
-        watch = ["SPY", "AAPL", "@BTC", "@ETH"]
-        wb.setup(watch, "1MIN")
-        self.assertEqual(wb.watch, watch)
-        self.assertEqual(wb.watch_stock, ["SPY", "AAPL"])
-        self.assertEqual(wb.watch_crypto, ["@BTC", "@ETH"])
+        watch = ["SPY", "@BTC"]
+        interval = {
+            "@BTC": {"interval": Interval.MIN_1, "aggregations": []},
+            "SPY": {"interval": Interval.MIN_1, "aggregations": []},
+        }
+        wb.setup(interval)
+        self.assertEqual(wb.watch_stock, ["SPY"])
+        self.assertEqual(wb.watch_crypto, ["@BTC"])
 
     @not_gh_action
     def test_main(self):
         def test_main(df):
-            self.assertEqual(len(df), 3)
+            self.assertEqual(len(df), 2)
             self.assertEqual(df["SPY"].columns[0][0], "SPY")
-            self.assertEqual(df["AAPL"].columns[0][0], "AAPL")
             self.assertEqual(df["@BTC"].columns[0][0], "@BTC")
 
         wb = Webull()
-        watch = ["SPY", "AAPL", "@BTC"]
-        wb.setup(watch, "1MIN", None, test_main)
+        watch = ["SPY", "@BTC"]
+        interval = {
+            "@BTC": {"interval": Interval.MIN_1, "aggregations": []},
+            "SPY": {"interval": Interval.MIN_1, "aggregations": []},
+        }
+        wb.setup(interval, None, test_main)
         wb.main()
 
     @not_gh_action
@@ -55,14 +75,16 @@ class TestWebull(unittest.TestCase):
 
         wb = Webull()
         watch = ["SPY"]
-        wb.setup(watch, "1MIN", None, test_main)
+        interval = {"SPY": {"interval": Interval.MIN_1, "aggregations": []}}
+        wb.setup(interval, None, test_main)
         wb.main()
 
     @not_gh_action
     def test_chain_info(self):
         wb = Webull()
         watch = ["SPY"]
-        wb.setup(watch, "1MIN", None, None)
+        interval = {"SPY": {"interval": Interval.MIN_1, "aggregations": []}}
+        wb.setup(interval)
         info = wb.fetch_chain_info("SPY")
         self.assertGreater(len(info["exp_dates"]), 0)
 
@@ -70,16 +92,68 @@ class TestWebull(unittest.TestCase):
     def test_chain_data(self):
         wb = Webull()
         watch = ["LMND"]
-        wb.setup(watch, "1MIN", None, None)
+        interval = {"LMND": {"interval": Interval.MIN_1, "aggregations": []}}
+        wb.setup(interval)
         dates = wb.fetch_chain_info("LMND")["exp_dates"]
         data = wb.fetch_chain_data("LMND", dates[0])
         self.assertGreater(len(data), 0)
         self.assertListEqual(list(data.columns), ["exp_date", "strike", "type", "id"])
-
         sym = data.index[0]
         df = wb.fetch_option_market_data(sym)
-
         self.assertTrue(True)
+
+    def test_order_option_limit(self):
+        dummy = PaperBroker()
+        dummy.streamer = Webull()
+        interval = {"A": {"interval": Interval.MIN_1, "aggregations": []}}
+        dummy.setup(interval)
+        exp_date = dt.datetime.now() + dt.timedelta(hours=5)
+        order = dummy.order_option_limit(
+            "buy", "A", 5, 50000, "OPTION", exp_date, 50001
+        )
+        self.assertEqual(order["type"], "OPTION")
+        self.assertEqual(order["id"], 0)
+        self.assertEqual(order["symbol"], "A")
+
+    def test_sell(self):
+        dummy = PaperBroker()
+        dummy.streamer = Webull()
+        interval = {"A": {"interval": Interval.MIN_1, "aggregations": []}}
+        dummy.setup(interval)
+        order = dummy.sell("A", 2)
+        self.assertEqual(order["type"], "STOCK")
+        self.assertEqual(order["id"], 0)
+        self.assertEqual(order["symbol"], "A")
+
+    def test_sell_order_limit(self):
+        dummy = PaperBroker()
+        dummy.streamer = Webull()
+        interval = {"A": {"interval": Interval.MIN_1, "aggregations": []}}
+        dummy.setup(interval)
+        order = dummy.order_limit("sell", "A", 2, 50000)
+        self.assertEqual(order["type"], "STOCK")
+        self.assertEqual(order["id"], 0)
+        self.assertEqual(order["symbol"], "A")
+
+    def test_buy(self):
+        dummy = PaperBroker()
+        dummy.streamer = Webull()
+        interval = {"A": {"interval": Interval.MIN_1, "aggregations": []}}
+        dummy.setup(interval)
+        order = dummy.buy("A", 5)
+        self.assertEqual(order["type"], "STOCK")
+        self.assertEqual(order["id"], 0)
+        self.assertEqual(order["symbol"], "A")
+
+    def test_buy_order_limit(self):
+        dummy = PaperBroker()
+        dummy.streamer = Webull()
+        interval = {"A": {"interval": Interval.MIN_1, "aggregations": []}}
+        dummy.setup(interval)
+        order = dummy.order_limit("buy", "A", 5, 50000)
+        self.assertEqual(order["type"], "STOCK")
+        self.assertEqual(order["id"], 0)
+        self.assertEqual(order["symbol"], "A")
 
 
 if __name__ == "__main__":
