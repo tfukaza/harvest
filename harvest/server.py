d598ca51fbd0d3dc04ec05bba569c1d78fef4ce2
Tomoki Fukazawa <tomokifukazawa123@gmail.com>
2021-09-28 23:28:12 -0700

    Update

--- harvest/server.py
+++ harvest/server.py
@@ -1,24 +1,19 @@
+import logging
 from flask import Flask, render_template, request, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 import threading
 import json
 
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 
 from harvest.utils import debugger
 
-class User:
+class User(UserMixin):
     def __init__(self, username, password):
-        self.username = username
+        self.id = username
         self.password = password
         self.default_password = True
-        self.is_authenticated = False 
-        self.is_active = True 
-        self.is_anonymous = False
-    
-    def get_id(self):
-        return self.username
 
 class DB:
     
@@ -31,7 +26,7 @@ class DB:
 
     def update_user_password(self, username, password):
         for user in self.users:
-            if user.username == username:
+            if user.get_id() == username:
                 user.password = generate_password_hash(password)
                 user.default_password = False
                 return True
@@ -39,7 +34,7 @@ class DB:
 
     def get_user(self, username):
         for user in self.users:
-            if user.username == username:
+            if user.get_id() == username:
                 return user
         return None
 
@@ -51,6 +46,7 @@ app = Flask(
 CORS(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "login"
 
 db = DB()  # Initiate database
 
@@ -73,11 +69,8 @@ class Server:
         )
         server.start()
 
-@app.route("/api/login", methods=["GET", "POST"])
-def login():
-
-    if request.method == "GET":
-        return render_template("minimal/login.html")
+@app.route("/api/login", methods=["POST"])
+def api_login():
 
     username = request.json["username"]
     password = request.json["password"]
@@ -89,50 +82,58 @@ def login():
         login_user(user)
         if user.default_password:
             return redirect("/update_password")
-        return {"message", "Success"}
+        else:
+            return redirect("/")
     else:
-        return {"message", "Failed"}
+        return json.dumps({"success": False})
 
-@app.route("/api/update_password", methods=["GET", "POST"])
+@app.route("/api/update_password", methods=["POST"])
 @login_required
-def update_password():
+def api_update_password():
 
-    if request.method == "GET":
-        return render_template("minimal/update_password.html")
+    username = current_user.get_id()
+    new_password = request.json["password"]
 
-    username = request.json["username"]
-    cur_password = request.json["cur_password"]
-    new_password = request.json["new_password"]
-
-    user = db.get_user(username)
-
-    if not user.default_password and not check_password_hash(
-        user.password, cur_password
-    ):
-        return {"message", "Incorrect password"}    
-    
     db.update_user_password(username, new_password)
+
+    logout_user()
+    return redirect("/login")
 
 @app.route("/api/logout", methods=["POST"])
 @login_required
-def logout():
+def api_logout():
     username = request.json["username"]
     user = db.get_user(username)
     logout_user(user)
 
-@login_manager.unauthorized_handler
-def unauthorized():
-    return redirect("/api/login")
-
-@login_manager.user_loader
-def load_user(user_id):
-    return db.get_user(user_id)
+@app.route("/api/crypto_positions")
+@login_required
+def api_crypto_positions():
+    return json.dumps(trader.crypto_positions)
 
 @app.route("/")
+@login_required
 def interface():
     return render_template("index.html")
 
-@app.route("/api/crypto_positions")
-def crypto_positions():
-    return json.dumps(trader.crypto_positions)
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("minimal/login.html")
 
+@app.route("/update_password", methods=["GET"])
+@login_required
+def update_password():
+    print("Render update password")
+    return render_template("minimal/update_password.html")
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    print("Need to log in")
+    return json.dumps({"message": "need to log in"})
+
+@login_manager.user_loader
+def load_user(user_id):
+    print(user_id)
+    u = db.get_user(user_id)
+    print(f"got {u.is_authenticated}")
+    return u
