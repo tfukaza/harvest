eac5b5c5a0d5e927b0287be7e176fb2315fc4ce3
Sher Shah <sher.shah010@berkeley.edu>
2021-11-07 21:17:07 -0800

    uncommented code

--- harvest/api/_base.py
+++ harvest/api/_base.py
@@ -212,20 +212,19 @@ class API:
         """
 
         def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-            # tries = 3
-            # while tries > 0:
-            #     try:
-            #         return func(*args, **kwargs)
-            #     except Exception as e:
-            #         self = args[0]
-            #         debugger.error(f"Error: {e}")
-            #         traceback.print_exc()
-            #         debugger.error("Logging out and back in...")
-            #         args[0].refresh_cred()
-            #         tries -= 1
-            #         debugger.error("Retrying...")
-            #         continue
+            tries = 3
+            while tries > 0:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    self = args[0]
+                    debugger.error(f"Error: {e}")
+                    traceback.print_exc()
+                    debugger.error("Logging out and back in...")
+                    args[0].refresh_cred()
+                    tries -= 1
+                    debugger.error("Retrying...")
+                    continue
 
         return wrapper
 
